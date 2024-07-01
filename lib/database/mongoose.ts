5272566b55bc5e510f56e2ff91cfef4f80629927
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-07-01 20:24:25 +0530

    modified:   lib/database/mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -24,7 +24,7 @@ export const connectToDatabase = async () => {
     cached.promise = 
       cached.promise || 
       mongoose.connect(MONGODB_URL, { 
-        dbName: 'Taskly', bufferCommands: false 
+        dbName: 'taskly', bufferCommands: false 
       })
   
     cached.conn = await cached.promise;
