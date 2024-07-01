3b7dfd509a6631f994f4cb54a23a8a987f16c367
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-07-01 20:56:16 +0530

    modified:   app/(root)/page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,17 @@
-import getUserById from '@/lib/actions/user.actions'
-import { connectToDatabase } from '@/lib/database/mongoose'
-import { auth } from '@clerk/nextjs/server'
-import React from 'react'
+import { connectToDatabase } from "@/lib/database/mongoose";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
 
 const HomePage = async () => {
-
-  const {userId} = auth()
-
-  const user = await getUserById(userId)
-
-  console.log(user)
-
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      HomePage
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
